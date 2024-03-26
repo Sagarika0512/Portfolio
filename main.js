2b1d5cde45812d49adef17e2a8fc47b0d3a256eb
Sagarika <100290405+Sagarika0512@users.noreply.github.com>
2024-03-26 22:58:48 +0530

    11th Update

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //CLIENT SIDE VALIDATION
 document.addEventListener("DOMContentLoaded", function () {
     var contactForm = document.getElementById("contact-form");
@@ -12,11 +11,13 @@ document.addEventListener("DOMContentLoaded", function () {
         contactForm.classList.add("was-validated");
     });
 });
-=======
+
 /*git branch -m branch main
 git fetch origin
 git branch -u origin/main main
 git remote set-head origin -a*/
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
->>>>>>> c1d69f273b13aef3bec29c95281185e1d4ab4baf
+
+
+  
