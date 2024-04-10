c3ec0d3314953465c2a1af8a799df7610fde8c58
Sagarika <100290405+Sagarika0512@users.noreply.github.com>
2024-04-10 20:59:14 +0530

    Removed .vscode files, updated .hintrc with axe/name-role-value rule and changed Portfolio subproject to a dirty state.
    The .vscode files removed are extensions.json, launch.json, and settings.json. The .hintrc file was updated with a new rule "axe/name-role-value" and an exception for "link-name". The Portfolio subproject was changed to a dirty state, indicating uncommitted changes.

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
+
+// lazyload.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lazyloadImages = document.querySelectorAll(".lazy");
+  
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const img = entry.target;
+          img.src = img.dataset.src;
+          img.classList.remove('lazy');
+          observer.unobserve(img);
+        }
+      });
+    });
+  
+    lazyloadImages.forEach(img => {
+      observer.observe(img);
+    });
+  });
+  
+  
+
+
 //CLIENT SIDE VALIDATION
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
     var contactForm = document.getElementById("contact-form");
 
     contactForm.addEventListener("submit", function (event) {
@@ -15,9 +40,9 @@ document.addEventListener("DOMContentLoaded", function () {
 /*git branch -m branch main
 git fetch origin
 git branch -u origin/main main
-git remote set-head origin -a*/
+git remote set-head origin -a
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-
+*/
   
