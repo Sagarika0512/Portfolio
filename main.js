44909eea863e17bba025b89361f508293871e339
Sagarika <100290405+Sagarika0512@users.noreply.github.com>
2024-05-09 13:14:02 +0530

    14th Update

--- main.js
+++ main.js
@@ -1,48 +1,48 @@
 
-// lazyload.js
+// // lazyload.js
 
-document.addEventListener("DOMContentLoaded", function() {
-    const lazyloadImages = document.querySelectorAll(".lazy");
+// document.addEventListener("DOMContentLoaded", function() {
+//     const lazyloadImages = document.querySelectorAll(".lazy");
   
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          const img = entry.target;
-          img.src = img.dataset.src;
-          img.classList.remove('lazy');
-          observer.unobserve(img);
-        }
-      });
-    });
+//     const observer = new IntersectionObserver((entries, observer) => {
+//       entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//           const img = entry.target;
+//           img.src = img.dataset.src;
+//           img.classList.remove('lazy');
+//           observer.unobserve(img);
+//         }
+//       });
+//     });
   
-    lazyloadImages.forEach(img => {
-      observer.observe(img);
-    });
-  });
+//     lazyloadImages.forEach(img => {
+//       observer.observe(img);
+//     });
+//   });
   
   
 
 
-//CLIENT SIDE VALIDATION
-/*document.addEventListener("DOMContentLoaded", function () {
-    var contactForm = document.getElementById("contact-form");
+// //CLIENT SIDE VALIDATION
+// /*document.addEventListener("DOMContentLoaded", function () {
+//     var contactForm = document.getElementById("contact-form");
 
-    contactForm.addEventListener("submit", function (event) {
-        if (!contactForm.checkValidity()) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
+//     contactForm.addEventListener("submit", function (event) {
+//         if (!contactForm.checkValidity()) {
+//             event.preventDefault();
+//             event.stopPropagation();
+//         }
 
-        contactForm.classList.add("was-validated");
-    });
-});
+//         contactForm.classList.add("was-validated");
+//     });
+// });
 
-/*git branch -m branch main
-git fetch origin
-git branch -u origin/main main
-git remote set-head origin -a
+// /*git branch -m branch main
+// git fetch origin
+// git branch -u origin/main main
+// git remote set-head origin -a
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+// <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-*/
+// */
   
